e6e2844931080fec29c7577cafca2214a276582f
Chaiwtun <chaiwtun@scg.com>
2021-06-20 10:20:49 +0700

    [database] connect simple database

--- src/main.go
+++ src/main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/NickUseGitHub/url-shortener/src/models"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
+	dsn := "host=localhost user=postgres password=postgres dbname=shortenurl port=5432 sslmode=disable TimeZone=Asia/Bangkok"
+	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		fmt.Println("[X]:: DB not connect")
+	}
+
+	fmt.Println("[√]:: DB Connected")
+
 	routes := mux.NewRouter()
 
 	routes.HandleFunc("/shorten-url", func(w http.ResponseWriter, r *http.Request) {
